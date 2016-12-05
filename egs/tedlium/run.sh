4cc2fd5d8719d1f8eba4702d6fc590225a672b36
Wang Jian <wantee.wang@gmail.com>
2016-12-05 19:31:12 +0800

    finish compute-ppl & add egs/tedlium

---
+++ egs/tedlium/run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+source path.sh
+
+num_dev_sentences=15000
+# num_cantab_TEDLIUM_sentences=250000
+vocab_size=100000
+disambig='#0'
+
+if [ ! -d cantab-TEDLIUM ]; then
+    echo "Downloading \"http://cantabresearch.com/cantab-TEDLIUM.tar.bz2\". "
+    wget --no-verbose --output-document=- http://cantabresearch.com/cantab-TEDLIUM.tar.bz2 | bzcat | tar --extract --file=- || exit 1
+    gzip cantab-TEDLIUM/cantab-TEDLIUM-pruned.lm3
+fi
+lm=cantab-TEDLIUM/cantab-TEDLIUM-pruned.lm3.gz
+
+if [ ! -d data ]; then
+  mkdir data
+fi
+
+head -n $num_dev_sentences < cantab-TEDLIUM/cantab-TEDLIUM.txt | sed 's/ <\/s>//g'  > data/dev.txt
+
+gunzip -c $lm |\
+  arpa2fst --disambig-symbol=$disambig --write-symbol-table=data/words.txt - data/G.fst
+disambig_id=grep '$disambig' data/words.txt | awk '{print $1}'
+
+sym2int.pl --map-oov '<unk>' data/words.txt < data/dev.txt > data/dev.int
+
+cat data/dev.int | sent-to-fst.py --disambig-symbol-id=$disambig_id | compute-ppl data/G.fst
+
+# With 15000 dev sentences and 250000 cantap-TEDLIUM sentences:
+# srilm perplexity:
+# ngram -lm cantab-TEDLIUM/cantab-TEDLIUM-pruned.lm3.gz -ppl data/dev.txt
+# ppl= 132.944 ppl1= 172.832
+
+# fstlm perplexity:
+# ppl=131.065
+# it is slightly different with srilm, since sometimes the backoff arc could be
+# better than non-backoff arc.
--- src/compute-ppl.cc
+++ src/compute-ppl.cc
@@ -37,9 +37,8 @@
 
 
 /**
-   This program loads in a fstLM and reads the sentences fsts from stdin,
-   then computes the perplexity for each sentence fsts and prints them to
-   the stdout.
+   This program loads in a fstLM and reads the sentences FSTs from stdin,
+   then computes the perplexity for sentence FSTs and prints it to the stdout.
 
 */
 
@@ -51,10 +50,14 @@ class PPLComputer {
   PPLComputer(int argc,
                const char **argv):
       total_log_prob_(0.0),
-      total_count_(0) {
+      total_count_(0), cur_sent_id_(0) {
 
     assert(argc >= 2);
     lm_ = VectorFst<StdArc>::Read(argv[1]);
+    if (!lm_) {
+      std::cerr << "compute-ppl: Reading LM FST error from" << argv[1] << '\n';
+      exit(1);
+    }
 
     ProcessInput();
     ProduceOutput();
@@ -67,42 +70,91 @@ class PPLComputer {
 
   void ProcessInput() {
     while (std::cin.peek(), !std::cin.eof()) {
+      int32 num_words;
+      std::cin >> num_words;
+
       VectorFst<StdArc> *fst = ReadNextFst();
-      ProcessCurrentFst(fst);
+      ProcessCurrentFst(*fst);
       delete fst;
+
+      cur_sent_id_++;
+      total_count_ += num_words;
     }
   }
 
   // this function reads next fst from stdin
   VectorFst<StdArc>* ReadNextFst() {
     fst::FstHeader hdr;
-    if (!hdr.Read(std::cin, "stdin"))
-      std::cerr << "Reading FST: error reading FST header";
+    if (!hdr.Read(std::cin, "stdin")) {
+      std::cerr << "compute-ppl: Sent " << cur_sent_id_
+          << ": Error reading sentence FST header\n";
+      exit(1);
+    }
     FstReadOptions ropts("<unspecified>", &hdr);
     VectorFst<StdArc> *fst = VectorFst<StdArc>::Read(std::cin, ropts);
-    if (!fst)
-      std::cerr << "Could not read sentenct fst";
+    if (!fst) {
+      std::cerr << "compute-ppl: Sent " << cur_sent_id_
+          << ": Could not read sentenct fst\n";
+      exit(1);
+    }
     return fst;
   }
 
-  void ProcessCurrentFst(VectorFst<StdArc> *fst) {
+  void ProcessCurrentFst(VectorFst<StdArc> &fst) {
       VectorFst<StdArc> comp_fst;
       VectorFst<StdArc> ofst;
 
-      Compose(*lm_, *fst, &comp_fst);
+      Compose(*lm_, fst, &comp_fst);
 
       ShortestPath(comp_fst, &ofst);
 
-      ofst.Write("1.fst");
+      double log_prob = GetProb(ofst);
+
+      total_log_prob_ += log_prob;
+
+#ifdef _FSTLM_DEBUG_
+      std::ostringstream oss;
+      oss << cur_sent_id_ << ".comp.fst";
+      comp_fst.Write(oss.str());
+      oss.str("");
+      oss << cur_sent_id_ << ".final.fst";
+      ofst.Write(oss.str());
+#endif
+  }
+
+  double GetProb(VectorFst<StdArc> &fst) {
+    typedef StdArc::StateId StateId;
+    typedef StdArc::Weight Weight;
+
+    StateId initial_state = fst.Start();
+    if (initial_state == kNoStateId) {
+      std::cerr << "compute-ppl: Sent " << cur_sent_id_
+          << ": empty FST after composition\n";
+      return 0.0;
+    }
+
+    Weight weight = Weight::One();
+
+    for (StateIterator<StdFst> siter(fst); !siter.Done(); siter.Next()) {
+      StateId st = siter.Value();
+      for (ArcIterator<StdFst> aiter(fst, st); !aiter.Done(); aiter.Next()) {
+        const StdArc &arc = aiter.Value();
+        weight = Times(weight, arc.weight);
+      }
+      if (fst.Final(st) != Weight::Zero()) {
+          weight = Times(weight, fst.Final(st));
+      }
+    }
+
+    return weight.Value();
   }
 
   void ProduceOutput() {
-    double ppl = exp(-total_log_prob_ / total_count_);
+    double ppl = exp(total_log_prob_ / total_count_);
     std::cout << std::setprecision(10) << ppl << "\n";
     std::cerr << "compute-ppl: average log-prob per word was "
               << (total_log_prob_ / total_count_)
-              << " (perplexity = "
-              << exp(-total_log_prob_ / total_count_) << ") over "
+              << " (perplexity = " << ppl << ") over "
               << total_count_ << " words.\n";
   }
 
@@ -111,6 +163,7 @@ class PPLComputer {
 
   double total_log_prob_;
   int64 total_count_;
+  int64 cur_sent_id_;
 };
 
 }  // namespace fstlm
@@ -118,11 +171,11 @@ class PPLComputer {
 int main (int argc, const char **argv) {
   if (argc < 2) {
     std::cerr << "usage:\n"
-              << "compute-ppl <lm_fst> \n"
+              << "compute-ppl <lm_fst> < <sent_fsts>\n"
               << "This program reads in a fst LM and compute the ppl for "
-              << "sentence fsts from stdin, then prints the total perplexity "
+              << "sentence FSTs from stdin, then prints the total perplexity "
               << "to stdout.\n"
-              << "The sentence fsts are typically built by sent-to-fst.py.\n";
+              << "The sentence FSTs are typically built by sent-to-fst.py.\n";
     exit(1);
   }
 

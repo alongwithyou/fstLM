d811dc884094d17359687d6755bfdee33b284c36
Wang Jian <wantee.wang@gmail.com>
2017-02-23 19:21:27 +0800

    fix command line for connlm-tofst in egs/connlm

--- egs/connlm/run.sh
+++ egs/connlm/run.sh
@@ -6,7 +6,7 @@ set -o pipefail
 source path.sh
 
 # data/g.txt and data/words.txt are generated from
-#connlm-tofst --boost=0.005 --boost-power=0.5 --word-syms-file=exp/rnn/words.txt --output-unk=true exp/rnn/final.clm exp/rnn/g.txt
+#connlm-tofst --word-selection-method=majority --max-gram=4 --ws-arg=0.5 --wildcard-ws-arg=0.9 --word-syms-file=exp/rnn/fst/words.txt --output-unk=true exp/rnn/final.clm exp/rnn/fst/g.txt
 
 fstcompile data/g.txt | fstarcsort - data/g.fst
 
@@ -14,3 +14,5 @@ fstcompile data/g.txt | fstarcsort - data/g.fst
                         --bos-symbol '<s>' \
                         --eos-symbol '</s>' \
                         data/g.fst data/words.txt data/test.txt
+
+# PPL is 94.8192

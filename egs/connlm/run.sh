de8350102dd5b327c90f56e90e0341866f48593c
Wang Jian <wantee.wang@gmail.com>
2017-04-18 22:45:49 +0800

    update egs/connlm to work with g.txt.gz

--- egs/connlm/run.sh
+++ egs/connlm/run.sh
@@ -5,14 +5,13 @@ set -o pipefail
 
 source path.sh
 
-# data/g.txt and data/words.txt are generated from
-#connlm-tofst --word-selection-method=majority --max-gram=4 --ws-arg=0.5 --wildcard-ws-arg=0.9 --word-syms-file=exp/rnn/fst/words.txt --output-unk=true exp/rnn/final.clm exp/rnn/fst/g.txt
+# see connLM/egs/tiny/run.sh 9 for generating data/g.txt.gz and data/words.txt
 
-fstcompile data/g.txt | fstarcsort - data/g.fst
+gunzip -c data/g.txt.gz | fstcompile | fstarcsort - data/g.fst
 
 ../steps/compute-ppl.sh --disambig-symbol '#phi' \
                         --bos-symbol '<s>' \
                         --eos-symbol '</s>' \
                         data/g.fst data/words.txt data/test.txt
 
-# PPL is 94.8192
+# PPL is 107.093

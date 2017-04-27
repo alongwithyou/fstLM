7ab4cec2096ea8fd632697f2aec9f2551df573e7
Wang Jian <wantee.wang@gmail.com>
2017-04-27 20:52:43 +0800

    compile fst with syms from connlm

--- egs/connlm/run.sh
+++ egs/connlm/run.sh
@@ -7,7 +7,8 @@ source path.sh
 
 # see connLM/egs/tiny/run.sh 9 for generating data/g.txt.gz and data/words.txt
 
-gunzip -c data/g.txt.gz | fstcompile | fstarcsort - data/g.fst
+gunzip -c data/g.txt.gz | fstcompile --isymbols=data/words.txt --osymbols=data/words.txt \
+                        | fstarcsort - data/g.fst
 
 ../steps/compute-ppl.sh --disambig-symbol '#phi' \
                         --bos-symbol '<s>' \
